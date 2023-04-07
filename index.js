bf5614a45c0708c7eea6c14eb9c1a0736199e975
Danylo Zymbalevskyi <zymbalevskyy@gmail.com>
2023-04-07 11:44:27 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+function introduction(name) {
+  console.log(`Hi, my name is ${name}.`);
+}
+
+function introductionWithLanguage(name, language) {
+  console.log(
+    `Hi, my name is ${name} and I am learning to program in ${language}.`
+  );
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  console.log(
+    `Hi, my name is ${name} and I am learning to program in ${language}.`
+  );
+}
+// check
+// introduction("dan");
+// introductionWithLanguage("lisa", "Assembly");
+// introductionWithLanguageOptional("ben", "Java");
+// introductionWithLanguageOptional("bob");
